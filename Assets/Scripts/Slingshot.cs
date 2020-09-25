37284a3f24c14bf1e5888d5ad90a0c42d95e5224
Scott Cochrane <scottii@cryptosecuregroup.com>
2020-09-25 00:15:04 -0400

    Fully finished camera.

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -59,6 +59,7 @@ public class Slingshot : MonoBehaviour
             aimingMode = false;
             projectileRigidbody.isKinematic = false;
             projectileRigidbody.velocity = -mouseDelta * velocityMult;
+            FollowCam.POI = projectile;
             projectile = null;
         }
     }
