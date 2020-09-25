fc1018351afabd19c3e701971ebdef38b041a84b
Scott Cochrane <scottii@cryptosecuregroup.com>
2020-09-25 16:20:42 -0400

    The ball come back

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -9,15 +9,32 @@ public class FollowCam : MonoBehaviour
     public float easing = 0.05f;
     public Vector2 minXY = Vector2.zero;
     [Header("Set Dynamically")]
-    public float camZ; 
+    public float camZ;
     void Awake()
     {
         camZ = this.transform.position.z;
     }
     void FixedUpdate()
     {
-        if (POI == null) return;
-        Vector3 destination = POI.transform.position;
+        //if (POI == null) return;
+        //Vector3 destination = POI.transform.position;
+        Vector3 destination;
+        if (POI == null)
+        {
+            destination = Vector3.zero;
+        }
+        else
+        {
+            destination = POI.transform.position;
+            if (POI.tag == "Projectile")
+            {
+                if (POI.GetComponent<Rigidbody>().IsSleeping())
+                {
+                    POI = null;
+                    return;
+                }
+            }
+        }
         destination.x = Mathf.Max(minXY.x, destination.x);
         destination.y = Mathf.Max(minXY.y, destination.y);
         destination = Vector3.Lerp(transform.position, destination, easing);
